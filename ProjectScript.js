080ff8a48a57e8eff3951ae4e6a1d211c6cf5507
Wesley Wei <wesley631w@gmail.com>
2018-01-26 00:47:05 -0800

    Modified DOM of projects and added styling.

--- ProjectScript.js
+++ ProjectScript.js
@@ -1,3 +1,8 @@
-function displayProject() {
-    
+function displayProject(divID) {
+    var projectDetails = document.getElementById(divID);
+    // projectDetails becomes hidden if unhidden and vice versa
+    if (projectDetails) {
+        projectDetails.className = (projectDetails.className=='hidden') ?
+        'unhidden' : 'hidden';
+    }
 }
