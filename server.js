e56d5b8103afbfab733c0e23bd1e0cb4d49c887b
Wesley Wei <wesley631w@gmail.com>
2019-04-20 20:51:02 -0700

    Added basic node structure for web app.

--- server.js
+++ server.js
@@ -1,138 +1,15 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
-const async = require("async");
+const express = require('express');
 const app = express();
 
-app.set("view engine", "pug");
-app.use(express.static("public"));
-app.use(express.static("public/js"));
-app.use(express.static("public/css"));
-app.use(express.static("node/modules"));
-app.use(bodyParser.urlencoded({ extended: true }));
+// Set view engine for node application
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+app.use(express.static('public/css'));
+app.use(express.static('public/images'));
 
-var resumeUrl = "mongodb://WesleyWei:WesleyResume@ds117759.mlab.com:17759/resume";
-var projectsUrl = "mongodb://WesleyWei:WesleyProjects1@ds125871.mlab.com:25871/wesprojects";
-var videosUrl = "mongodb://WesleyWei:WesleyVideos1@ds131551.mlab.com:31551/wesvideos";
-
-var resumeDb;
-var projectsDb;
-var videosDb;
-
-MongoClient.connect(resumeUrl, (err, client) => {
-    if (err) {
-        return console.log(err);
-    }
-    resumeDb = client.db('resume');
-});
-
-MongoClient.connect(projectsUrl, (err, client) => {
-    if (err) {
-        return console.log(err);
-    }
-    projectsDb = client.db('wesprojects');
-});
-
-MongoClient.connect(videosUrl, (err, client) => {
-    if (err) {
-        return console.log(err);
-    }
-    videosDb = client.db('wesvideos');
-});
-
-app.get("/", (req, res) => {
-    res.render("WesleyHome");
-});
-
-app.get("/WesleyResume", (req, res) => {
-    var locals = {};
-    locals.title = "WestWay";
-    locals.pageType = "Projects";
-    locals.css = "WesleyResume.css";
-    var tasks = [
-        // Load work experience
-        function (callback) {
-            resumeDb.collection("workExperience").find().toArray((err, results) => {
-                if (err) {
-                    return console.log(err);
-                }
-                locals.workExperience = results;
-                callback();
-            });
-        },
-
-        //Load skills
-        function (callback) {
-            resumeDb.collection("skills").find().toArray((err, results) => {
-                if (err) {
-                    return console.log(err);
-                }
-                locals.skills = results;
-                callback();
-            });
-        },
-
-        // Load leadership
-        function (callback) {
-            resumeDb.collection("leadership").find().toArray((err, results) => {
-                if (err) {
-                    return console.log(err);
-                }
-                locals.leadership = results;
-                callback();
-            });
-        },
-
-        // Load honors and awards
-        function (callback) {
-            resumeDb.collection("honorsAndAwards").find().toArray((err, results) => {
-                if (err) {
-                    return console.log(err);
-                }
-                locals.honorsAndAwards = results;
-                callback();
-            });
-        }
-    ];
-    
-    async.parallel(tasks, function(err) {
-        if (err) {
-            return next(err); // let express handle the error by calling 'next'
-        }
-        res.render("WesleyResume", locals)
-    });
-});
-
-app.get("/WesleyProjects", (req, res) => {
-    var locals = {};
-    locals.title = "WestWay";
-    locals.pageType = "Projects";
-    locals.css = "WesleyProject.css";
-    // Load projects
-    projectsDb.collection("projects").find().toArray((err, results) => {
-        if (err) {
-            return console.log(err);
-        }
-        locals.projects = results;
-        res.render("WesleyProjects", locals);
-    });
-});
-
-app.get("/WesleyVideos", (req, res) => {
-    var locals = {};
-    locals.title = "WestWay";
-    locals.pageType = "Videos";
-    locals.css = "WesleyVideo.css";
-    // Load videos
-    videosDb.collection("videos").find().toArray((err, results) => {
-        if (err) {
-            return console.log(err);
-        }
-        locals.videos = results;
-        res.render("WesleyVideos", locals);
-    });
-});
+// Use Controllers
+app.use(require('./controllers/index'));
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Wesley's app listening on port 3000!");
+    console.log('Project Wesley Wei server is running');
 });
