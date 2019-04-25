b0ddad65b22a63d201e389275621dbe9cc72bb3c
Wesley Wei <wesley631w@gmail.com>
2019-04-25 03:29:53 -0700

    Added new font and navbar with positioning. Need to make responsive still.

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+// Typed.js implementation for the main section text
 var options = {
     strings: [
         'My name is <i>Wesley Wei</i>. I am the best developer in the world!'
@@ -5,4 +6,22 @@ var options = {
     typeSpeed: 50
 };
 
-var typed = new Typed('#header-text', options);
+var typed = new Typed('#main div p', options);
+
+// Hovering effects for navbar
+var navButtonElem = document.getElementsByClassName('nav-button');
+var navBarSectionDiv = document.getElementsByClassName('navbar-section');
+var navBarSectionContentDiv = document.getElementsByClassName(
+    'navbar-section-content'
+);
+for (let i = 0; i < navButtonElem.length; i++) {
+    navButtonElem[i].addEventListener('mouseover', () => {
+        navBarSectionDiv[i].style.backgroundColor = 'white';
+        navBarSectionContentDiv[i].classList.remove('hidden');
+    });
+
+    navButtonElem[i].addEventListener('mouseout', () => {
+        navBarSectionDiv[i].style.backgroundColor = 'transparent';
+        navBarSectionContentDiv[i].classList.add('hidden');
+    });
+}
