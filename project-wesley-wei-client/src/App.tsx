44e3227fa8feb0b6a245f5bb1eec3f59c091c1ef
Wesley Wei <wesley631w@gmail.com>
2022-05-22 20:27:09 -0400

    Added Ocean WebGL plane to landing page - Added React Three Fiber and Drei - Updated all Package.json versions - Updated Index root rendering because of old React DOM deprecated code

--- project-wesley-wei-client/src/App.tsx
+++ project-wesley-wei-client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { observer } from 'mobx-react';
 import { RootStoreContext } from './index';
 import Landing from './components/features/Layout/Landing';
@@ -9,9 +9,9 @@ const App = () => {
 	const RootStore = useContext(RootStoreContext);
 	return (
 		<Router>
-			<Switch>
-				<Route exact path="/" component={Landing} />
-			</Switch>
+			<Routes>
+				<Route path="/" element={<Landing />} />
+			</Routes>
 		</Router>
 	);
 };
--- project-wesley-wei-client/src/components/features/Layout/Landing.tsx
+++ project-wesley-wei-client/src/components/features/Layout/Landing.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Compass from '../Compass/Compass';
 import Navbar from '../Navigation/Navbar';
+import Ocean from '../3DModels/Ocean';
+import { Canvas } from '@react-three/fiber';
+import { Sky } from '@react-three/drei';
 
 const Landing = () => {
 	return (
 		<>
+			<Canvas style={{ width: "100vw", height: "100vh" }} camera={{ position: [ 0, 10, 20 ] }}>
+				<Ocean />			
+				<Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
+			</Canvas>
 			<header>
 				<div>
 					<img
