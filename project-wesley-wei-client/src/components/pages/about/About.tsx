d8f1168e5b083b737b77509009b8f739476781c2
Wesley Wei <wesley631w@gmail.com>
2023-04-05 18:27:10 +0800

    Added About component with blurred about background image - Added about href target - Added new aboutMe image - Added About styles

---
+++ project-wesley-wei-client/src/components/pages/about/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/About.css';
+
+const About = () => {
+    return (
+        <div id="about">
+            <img className="about-bg-cover" src="/assets/images/aboutMe2.jpeg" alt="" />
+        </div>
+    );
+};
+
+export default About;
--- project-wesley-wei-client/src/components/pages/landing/Landing.tsx
+++ project-wesley-wei-client/src/components/pages/landing/Landing.tsx
@@ -5,13 +5,14 @@ import Ocean from 'components/common/3DModels/Ocean';
 import Header from 'components/layout/header/Header';
 import { Canvas } from '@react-three/fiber';
 import { Sky } from '@react-three/drei';
+import About from '../about/About';
 import './styles/Landing.css';
 
 const Landing = () => {
 	return (
 		<>
 			<div className="landing-container">
-				<Canvas style={{ width: "100vw", height: "100vh" }} camera={{ position: [ 0, 10, 20 ] }}>
+				<Canvas style={{ width: "100%", height: "100vh" }} camera={{ position: [ 0, 10, 20 ] }}>
 					<Ocean />			
 					<Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
 				</Canvas>
@@ -22,13 +23,14 @@ const Landing = () => {
 				<Navbar
 					className="westway-navbar"
 					navSections={[
-						{ name: 'About', href: '/about' },
+						{ name: 'About', href: '#about' },
 						{ name: 'Projects', href: '/projects' },
 						{ name: 'Resume', href: '/resume' },
 						{ name: 'Videos', href: '/videos' }
 					]}
 				/>
 			</div>
+			<About />
 		</>
 	);
 };
