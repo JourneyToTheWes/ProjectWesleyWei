43c6162a7d40a1b8f5b7c43be6cacd2e35bd4d49
Wesley Wei <wesley631w@gmail.com>
2023-04-06 16:04:17 +0800

    Made the Header position fixed and added background and foreground blocks for the second About section

--- project-wesley-wei-client/src/components/pages/about/About.tsx
+++ project-wesley-wei-client/src/components/pages/about/About.tsx
@@ -4,21 +4,28 @@ import './styles/About.css';
 const About = () => {
     return (
         <div id="about">
-            <img
-                className="about-bg-cover"
-                src="/assets/images/aboutMe2.jpeg"
-                alt="About section background cover image"
-            />
-            <img
-                className="about-fg-img"
-                src="/assets/images/aboutMe2.jpeg"
-                alt="About section foreground (about me) image"
-            />
-            <h1 className="about-header">
-                Hello! My name is <br/>
-                <span>Wesley Wei</span> and <br/>
-                this is <span>West Way</span>.
-            </h1>
+            <div className="about-section-one">
+                <img
+                    className="about-bg-cover"
+                    src="/assets/images/aboutMe2.jpeg"
+                    alt="About section background cover image"
+                />
+                <img
+                    className="about-fg-img"
+                    src="/assets/images/aboutMe2.jpeg"
+                    alt="About section foreground (about me) image"
+                />
+                <h1 className="about-header">
+                    Hello! My name is <br/>
+                    <span>Wesley Wei</span> and <br/>
+                    this is <span>West Way</span>.
+                </h1>
+            </div>
+            <div className="about-section-two">
+                <div className="bg-block"></div>
+                <div className="fg-block-1"></div>
+                <div className="fg-block-2"></div>
+            </div>
         </div>
     );
 };
