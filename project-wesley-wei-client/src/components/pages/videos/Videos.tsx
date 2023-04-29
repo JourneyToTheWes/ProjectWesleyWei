aa01da2e873f37870a24e2723455c76e976b9087
Wesley Wei <wesley631w@gmail.com>
2023-04-28 20:31:23 -0400

    Added the main video section to the Videos component

--- project-wesley-wei-client/src/components/pages/videos/Videos.tsx
+++ project-wesley-wei-client/src/components/pages/videos/Videos.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RootStore from 'stores/RootStore';
 import { observer } from 'mobx-react';
+import Header from 'components/layout/header/Header';
 import './styles/Videos.css';
+import Compass from 'components/common/compass/Compass';
 
 interface IVideos {
     store: RootStore;
@@ -9,10 +11,28 @@ interface IVideos {
 
 const Videos: React.FC<IVideos> = ({ store }) => {
     const videoStore = store.VideoStore;
+    const [mainVideoUrl, setMainVideoUrl] = useState<string>(videoStore.videos[0].url);
+
     return (
         <>
-            <h1>Videos!!!</h1>
-            <h1>{videoStore.areVideosLoaded ? 'videos loaded' : 'videos not loaded'}</h1>
+            <Header
+                children={<Compass size="small" />}
+            />
+            <div className="main-video-section">
+                <div className="main-video-section-left-panel">
+                    <h1>Videos</h1>
+                    <p>
+                        This content highlights activities/hobbies I pursue in a video format.
+                        Below is a library of my videos. Click on any of the videos to be
+                        entertained or learn more about me...
+                    </p>
+                </div>
+                <div className="main-video-container">
+                    {
+                        videoStore.areVideosLoaded && <iframe src={mainVideoUrl}></iframe>
+                    }
+                </div>
+            </div>
         </>
     );
 };
