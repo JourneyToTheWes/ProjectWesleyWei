e291eb36733c63eb526cca7dcaec8dec8e6f6896
Wesley Wei <wesley631w@gmail.com>
2024-01-15 17:34:04 -0800

    Added all Fly.io required changes in order for deployment to work - Commented out app.ts changes that are uncommented to deploy

--- project-wesley-wei-server/app.ts
+++ project-wesley-wei-server/app.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import { Application } from 'express';
+import * as path from 'path';
 type Port = string | number;
 interface IAppInit {
     port: Port;
@@ -13,9 +14,29 @@ class App {
 
     constructor(appInit: IAppInit) {
         this.app = express();
+        
+        // These two lines are to serve the static React build project that has been manually
+        // copied over to the server directory '/public' folder. The second line is to match
+        // any route other than '/api*' to use the React index html build file as it the client
+        // app cannot route as it normally does locally. Any requests to any route (other than '/api*')
+        // would be served index.html.
+        // Note: uncomment out these two lines when building the server directory and deploying the service
+        // so that the full-stack applicant can work seamlessly.
+        // this.app.use(express.static(path.join(__dirname + '/build/public')));
+        // this.app.use(express.static(path.join(__dirname + '/../public')));
+        // this.app.get('^(?!\/?api).+$', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+        // this.app.get('^(?!\/?api).+$', (req, res) => res.sendFile(path.join(__dirname + '/../public/index.html')));
+        // this.app.get(/^(http:\/\/)?[\w.:]+\/(?!\/?api).+$/gm, (req, res) => res.sendFile(path.join(__dirname + '/../public/index.html')));
+        
         this.port = appInit.port;
         this.useMiddleWares(appInit.middleWares);
         this.useRoutes(appInit.controllers);
+
+        // console.log("path join dirname: " + path.join(__dirname, '/../public', 'index.html'));
+        // this.app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '/build/public', 'index.html')));
+        // this.app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html')));
+        // this.app.get('^(?!\/?api).+$', (req, res) => res.sendFile(path.join(__dirname, '../public', 'index.html')));
+        // this.app.get(/^\/(?!api).*/, (req, res) => res.sendFile(path.join(__dirname + '/../public/index.html')));
     }
 
     private useMiddleWares = (middleWares: any[]) => {
--- project-wesley-wei-server/config/db.ts
+++ project-wesley-wei-server/config/db.ts
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, {
+        await mongoose.connect(process.env.mongoURI || config.get('mongoURI'), {
             useNewUrlParser: true,
 			useCreateIndex: true,
 			useFindAndModify: false,
