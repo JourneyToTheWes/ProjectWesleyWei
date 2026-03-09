aad51d33da279b191d63e952dcc2fafa62650d09
Wesley Wei <wesley631w@gmail.com>
2026-03-09 14:55:19 -0400

    Remove CRA frontend and migrate to Vite frontend - Added Tailwind and Framer Motion to new Vite React app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1 className="text-4xl font-bold">Wesley Wei</h1>
+        </div>
+    );
+};
+
+export default App;
